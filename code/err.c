2a0d025a1131c6d4c7775599b9fc1e3a6ff6073c
Paweł Krawczyk <pawel.krawczyk@student.uw.edu.pl>
2014-04-07 17:44:11 +0200

    dodano err.c err.h i uniwersalny makefile

---
+++ code/err.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include "err.h"
+
+void syserr(const char *fmt, ...)  
+{
+  va_list fmt_args;
+
+  fprintf(stderr, "ERROR: ");
+
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end (fmt_args);
+  fprintf(stderr," (%d; %s)\n", errno, strerror(errno));
+  exit(EXIT_FAILURE);
+}
+
+void fatal(const char *fmt, ...)
+{
+  va_list fmt_args;
+
+  fprintf(stderr, "ERROR: ");
+
+  va_start(fmt_args, fmt);
+  vfprintf(stderr, fmt, fmt_args);
+  va_end (fmt_args);
+
+  fprintf(stderr,"\n");
+  exit(EXIT_FAILURE);
+}
