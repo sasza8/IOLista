b87d7c29e966f80dee544cbfce3c11a29f68c54b
Paweł Krawczyk <pawel.krawczyk@student.uw.edu.pl>
2014-04-08 14:32:44 +0200

    <3 fajny commit :* powazny bardzo

--- code/client/client.cpp
+++ code/client/client.cpp
@@ -1,12 +1,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <stdio.h>
 
 #include "protocol/proto.h"
 #include <assert.h>
