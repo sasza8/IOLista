08132a292f98d528ffe8c93531882be225488185
Krzysztof Stopczański <krzysztof@stopczanski.pl>
2014-04-08 23:36:06 +0200

    serwer z pobieraniem i dodawaniem zadan

--- code/server/client.h
+++ code/server/client.h
@@ -29,21 +29,26 @@ public:
 	{
 		int parent;
 		string description;
-	}
+	};
 	
 	void setUserDetails(ListDatabase::ListUser dbUser);
 	
 	LoginDetails getLoginDetails();
 	RegisterDetails getRegisterDetails();
 	TaskDetails getTaskDetails();
+	int getTasksParent();
 	
 	void loginOK();
 	void loginFailed();
 	void registerOK();
+	void startSendingTasks(int number);
+	void sendTask(ListDatabase::ListTask task);
+	
 	void sendType(unsigned type);
 	unsigned getType(); // odbiera od klienta sam typ pakietu
 	
 	unsigned getID();
+	string getUsername();
 	
 	Client(int sock);
 	~Client();
