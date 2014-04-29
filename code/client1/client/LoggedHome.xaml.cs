84fcdebb34ae9fe88a1f3fd1d606c8983e6b7d48
Pawel Krawczyk <pawel.krawczyk@student.uw.edu.pl>
2014-04-29 14:34:04 +0200

    Dodano treeView i klase taskExplorer do przegladania /aktualizowania zadan

--- code/client1/client/LoggedHome.xaml.cs
+++ code/client1/client/LoggedHome.xaml.cs
@@ -26,7 +26,8 @@ namespace client
         private String token;
         private TcpClient client;
 
-        UberTask uberTask;
+        private UberTask uberTask;
+        private TaskExplorer taskExplorer;
 
         public LoggedHome(string _username, string _token, TcpClient _client)
         {
@@ -34,33 +35,34 @@ namespace client
             username = _username;
             token = _token;
             client = _client;
+            taskExplorer = new TaskExplorer();
         }
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             // FIXME
             // gdy okienko sie wczyta to wysyłamy rządanie o
-            // pierwsza warstwe taskow!
-            listViewTasks.Items.Add(new Task
-            {
-                id = 1,
-                createOn = "dzisiaj",
-                lastChange = "mnute temu",
-                done = false,
-                parent = 1,
-                description = @"
-                taki tam opis projektu, kpic cos"
-            });
-            listViewTasks.Items.Add(new Task
-            {
-                id = 2,
-                createOn = "wczoraj",
-                lastChange = "teraz",
-                done = true,
-                parent = 2,
-                description = @"
-                BLA BLA BLA BLA ploooo"
-            });
+//            // pierwsza warstwe taskow!
+//            listViewTasks.Items.Add(new Task
+//            {
+//                id = 1,
+//                createOn = "dzisiaj",
+//                lastChange = "mnute temu",
+//                done = false,
+//                parent = 1,
+//                description = @"
+//                taki tam opis projektu, kpic cos"
+//            });
+//            listViewTasks.Items.Add(new Task
+//            {
+//                id = 2,
+//                createOn = "wczoraj",
+//                lastChange = "teraz",
+//                done = true,
+//                parent = 2,
+//                description = @"
+//                BLA BLA BLA BLA ploooo"
+//            });
             try
             {
                 NetworkStream stream = client.GetStream();
@@ -78,7 +80,71 @@ namespace client
             }
         }
 
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            string response =
+                @"{
+                    'subtasks' :
+[
+{
+               'id' : '1',
+               'createOn': 'zisiaj',
+               'lastChange': 'mnute temu',
+               'done': 'False',
+               'parent': '1',
+               'description':  'opis',
+               'name' : 'posprzataj pokoj',
+               'subtasks' : [], 
+},
+{
+               'id' : '2',
+               'createOn': 'wczoraj',
+               'lastChange': 'mnute temu',
+               'done': 'True',
+               'parent': '1',
+               'description':  'blalblalbla opis',
+               'name' : 'name2',
+               'subtasks' : [], 
+},
+{
+               'id' : '3',
+               'createOn': 'zisiaj',
+               'lastChange': 'mnute temu',
+               'done': 'false',
+               'parent': '1',
+               'description':  'opis',
+               'name' : 'name3',
+               'subtasks' : [], 
+},
+],
+}";
+            uberTask = JsonConvert.DeserializeObject<UberTask>(response);
+            foreach(Task t in uberTask.subtasks)
+            {
+                taskExplorer.addNewTask(t, treeViewTaskExplorer);
+            }
+            foreach (Object ob in treeViewTaskExplorer.Items)
+            {
+                TreeViewItem node = ob as TreeViewItem;
+                if(node != null)
+                {
+                    node.ContextMenu = treeViewTaskExplorer.Resources["NodeContextMenu"]
+                        as System.Windows.Controls.ContextMenu;
+                }
+            }
+        }
 
+        // ***************** PRIVATE FUNCTIONS ************ //
+        // ************************************************ //
+
+        private void addMenusToTreeExplorer()
+        {
+            // TODO
+            foreach( TreeViewItem RootNode in treeViewTaskExplorer.Items)
+            {
+                //RootNode.Parent;
+            }
+        }
 
     } // class
 } // namespace
