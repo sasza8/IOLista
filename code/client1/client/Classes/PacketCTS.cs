c1e01303727dadfd9d83d4c4a0c6932e5a7e90f5
Pawel Krawczyk <pawel.krawczyk@student.uw.edu.pl>
2014-04-30 14:33:18 +0200

    zmieniono protokol (I hate you) znowu, pakiety DO i OD serwera

---
+++ code/client1/client/Classes/PacketCTS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class PacketCTS
+    {
+        public string type { get; set; }
+        public string authToken { get; set; }
+        public Dictionary<string, object> parameters { get; set; }
+
+        public PacketCTS()
+        {
+            parameters = new Dictionary<string,object>();
+        }
+    }
+}
